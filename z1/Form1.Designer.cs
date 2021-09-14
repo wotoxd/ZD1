7f7e1343209ca1ba2227df7e0cbec285c6d4edeb
Danila <ewik2k17@gmail.com>
2021-09-14 13:31:51 +0300

    com1
    zd1

---
+++ z1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace z1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(48, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(173, 43);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(710, 39);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "SORT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(173, 97);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(710, 39);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(173, 152);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(710, 39);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(173, 200);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(557, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "BUBLE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "SHAKER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "qSORT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1053, 350);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ z1/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace z1
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch s1 = new Stopwatch();
+        Stopwatch s2 = new Stopwatch();
+        Stopwatch s3 = new Stopwatch();
+
+        double tm1;
+        double tm2;
+        double tm3;
+        public Form1()
+        {
+            InitializeComponent();
+            tm1 = 0;
+            tm2 = 0;
+            tm3 = 0;
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        public class sort
+        {      
+
+            static void swap(int[] array, int i, int j)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            public static string Bsort(int[] b)
+            {
+                
+                long sw=0,pr =0;
+
+                string a;
+                for (int i = 0; i < b.Length; i++)
+                {
+                    for (int j = 0; j < b.Length - 1; j++)
+                    {
+                        sw++;
+                        if (b[j] > b[j + 1])
+                        {
+                            swap(b, j, j + 1);
+                            pr++;
+                            
+                        }
+                    }
+                }
+                a = "ОТСОРТИРОВАНО"+" "+"СРАВНЕНИЙ = "+ sw+ " "+"ПЕРЕСТАНОВОК = "+ pr;
+                return a;
+                
+
+            }
+
+            public static string SHsort(int[] mas)
+            {
+                long sw = 0, pr = 0;
+                string a1;
+                int l = 0, r = mas.Length - 1;
+
+                while (l<r)
+                {
+                    for (int i = l; i < r; i++)
+                    {
+                        sw++;
+                        if (mas[i]>mas[i+1])
+                        {
+                            swap(mas, i, i + 1);
+                            pr++;
+                        }
+                    }
+                    r--;
+                    for (int i = r; i > l; i--)
+                    {
+                        sw++;
+                        if (mas[i - 1] > mas[i])
+                        {
+                            swap(mas, i - 1, i);
+                            pr++;
+                        }
+                    }
+                    l++;
+                }
+                a1 = "ОТСОРТИРОВАНО" + " " + "СРАВНЕНИЙ = " + sw + " " + "ПЕРЕСТАНОВОК = " + pr;
+                return a1;
+            }
+
+            public static string Qsort(int[] bmas, int left, int right)
+            {
+                long sw = 0, pr = 0;
+                int i = left;
+                int k = right;
+                int p = bmas[(left + right) / 2];
+                do
+                {
+                    
+                    while (bmas[i] < p && i < right) { i++; sw++; } ;
+                    while (p < bmas[k] && k > left) { k--; sw++; } ;
+                    
+                    if (i<=k)
+                    {
+                        swap(bmas, i, k);
+                        i++;
+                        k--;
+                        pr++;
+                    }
+                } while (i<=k);
+                if (left < k) Qsort(bmas, left, k);
+                if (i < right) Qsort(bmas, i, right);
+                string ans = "ОТСОРТИРОВАНО" + " " + "СРАВНЕНИЙ = " + sw + " " + "ПЕРЕСТАНОВОК = " + pr;
+                return ans;
+            }
+        }
+        
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+
+            
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            if (textBox1.Text=="")
+            {
+                MessageBox.Show("Заполните поле ");
+                
+            }
+            
+            Random r = new Random();
+            int[] mas = new int[Convert.ToInt32(textBox1.Text)];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                int t = r.Next(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+
+                mas[i] += t;
+
+
+
+            }
+            
+           
+            int[] ms1=new int[mas.Length]; 
+            int[] ms2=new int[mas.Length]; 
+            int[] ms3=new int[mas.Length];
+            Array.Copy(mas, ms1, mas.Length);
+            Array.Copy(mas, ms2, mas.Length);
+            Array.Copy(mas, ms3, mas.Length);
+            s1.Start();
+
+            textBox4.Text = sort.Bsort(mas).ToString();
+            s1.Stop();
+            s2.Start();
+            textBox5.Text = sort.SHsort(ms2).ToString();
+            s2.Stop();
+            s3.Start();
+            textBox6.Text = sort.Qsort(ms3, 0, ms3.Length - 1);
+            s3.Stop();
+            TimeSpan ts1 = s1.Elapsed;
+            TimeSpan ts2 = s2.Elapsed;
+            TimeSpan ts3 = s3.Elapsed;
+            string et1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts1.Hours, ts1.Minutes, ts1.Seconds,
+            ts1.Milliseconds / 10);
+            string et2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts2.Hours, ts2.Minutes, ts2.Seconds,
+            ts2.Milliseconds / 10);
+            string et3 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts3.Hours, ts3.Minutes, ts3.Seconds,
+            ts3.Milliseconds / 10);
+            textBox7.Text = "Время на сортировку 1,2,3 массива :" + et1 + "\t" + et2 + "\t" + et3;
+
+            
+
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    textBox6.Text += ms3[i].ToString() + " ";
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
